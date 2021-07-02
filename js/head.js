4e16962fca27aacaac84aa06a268469930d3d1c4
Mats Berg <mats.berg@mau.se>
2021-07-02 17:29:23 +0200

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,5 +1,5 @@
-  var link = document.createElement('meta');
-  link.setAttribute('name', 'viewport');
-  link.content = "width=device-width, initial-scale=1.0";
-  document.getElementsByTagName('head')[0].appendChild(link);
+  var styleref = document.createElement('link');
+  styleref.setAttribute('rel', 'stylesheet');
+  styleref.href = "css/style.css";
+  document.getElementsByTagName('head')[0].appendChild(styleref);
 
