f738c270c6ae2008d4cf755d1ced526ae66e13ef
Mats Berg <mats.berg@mau.se>
2021-07-02 16:50:28 +0200

    Create head.js

---
+++ js/head.js
@@ -0,0 +1,5 @@
+  var link = document.createElement('meta');
+  link.setAttribute('name', 'viewport');
+  link.content = "width=device-width, initial-scale=1.0";
+  document.getElementsByTagName('head')[0].appendChild(link);
+
