562847e3c14909ee5b1feb2736967e2d3058fc6b
Mats Berg <a.mats.berg@gmail.com>
2023-11-26 15:49:57 +0100

    added testtom

--- js/head.js
+++
@@ -1,9 +0,0 @@
-  var metatag = document.createElement('meta');
-  metatag.setAttribute('name', 'viewport');
-  metatag.content = "width=device-width, initial-scale=1.0";
-  document.getElementsByTagName('head')[0].appendChild(metatag);
-
- var linktag = document.createElement('link');
-  linktag.setAttribute('rel', 'stylesheet');
-  linktag.href = "../css/style.css";
-  document.getElementsByTagName('head')[0].appendChild(linktag);
